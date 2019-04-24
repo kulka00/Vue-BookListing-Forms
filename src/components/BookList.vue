00cca7a803a62a32d91dbe1495bb4a1ededf52e5
Kulkarni, Ankita A <patilankita63@gmail.com>
2019-04-24 13:48:17 +0200

    chenges

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,12 +4,22 @@
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
+    <hr>
+    <h2>Filters Book By onwership</h2><br>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{filter}}</option>
+    </select>
+    <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
+    </hr>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
+import _ from "lodash"
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -20,19 +30,26 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson",finishedReading:true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading:false},
-        { title: "Amusing Ourselves to Death", author: "Neil Postman",finishedReading:true }
-      ]
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson",finishedReading:true,onwership:"borrowed" },
+        { title: "American Gods", author: "Neil Gaiman", finishedReading:false,onwership:"bought"},
+        { title: "Amusing Ourselves to Death", author: "Neil Postman",finishedReading:true,onwership:"borrowed" }
+      ],
+      filters:["bought","borrowed"],
+      holding:"bought"
     };
   },
   components: {
     BookItem,
     BookForm
   },
+  computed:{
+    filteredBooks (){
+      return _.filter(this.books,["onwership",this.holding]);
+    }
+  },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor,finishedReading:bookData.finishedReading });
+      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor,finishedReading:bookData.finishedReading,onwership:bookData.onwership });
     }
   }
 };
